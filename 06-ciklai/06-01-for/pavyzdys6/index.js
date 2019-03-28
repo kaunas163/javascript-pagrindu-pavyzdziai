18d7fbf46fe801c74e616b5fe3de8cf029c6d4b9
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-03-28 08:17:21 +0200

    pataisymai

--- 06-ciklai/06-01-for/pavyzdys6/index.js
+++ 06-ciklai/06-01-for/pavyzdys6/index.js
@@ -1,4 +1,4 @@
-let sum = 10;
+let sum = 0;
 
 for (let i = 1; i < 100; i++) {
     if (i % 5 === 0) {
--- 06-ciklai/06-04-for-in/pavyzdys9/index.js
+++ 06-ciklai/06-04-for-in/pavyzdys9/index.js
@@ -2,7 +2,6 @@ let marks = [4, 7, 8, 3, 4, 7, 8, 9, 10, 3];
 let positive = [];
 let negative = [];
 
-
 for (let key in marks) {
     if (marks[key] >= 5) {
         positive[positive.length] = marks[key];
