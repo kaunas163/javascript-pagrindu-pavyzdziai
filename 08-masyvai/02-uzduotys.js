4473ddb81d825ae2802a3d04bde06f05727739ce
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-04-02 17:05:57 +0300

    uzduotys

---
+++ 08-masyvai/02-uzduotys.js
@@ -0,0 +1,16 @@
+/* 1. Sukurkite masyvą su 5-iais norimais skaičiais.
+Į šio masyvo galą pridėkite dar du skaičius.
+Į šio masyvo priekį pridėkite tris raides.
+Į masyvą, nuo antros pozicijos pridėkite raidę a.
+Į masyvą, nuo penktos pozicijos pridėkite raides b, c, d.
+Masyvą išveskite po jo sukūrimo ir kiekvieno atlikto veiksmo.
+*/
+
+
+
+/* 2. Sukurkite masyvą su 10 skirtingų skaičių.
+Į masyvo vidurį įkelkite dar 5 atsitiktinius skaičius.
+Raskite skaičių vidurkį.
+Viską išveskite konsolėje.
+*/
+
