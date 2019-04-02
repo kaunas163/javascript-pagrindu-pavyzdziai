08426b8d43a1ceb6b56faa47c51ea51be7b685a3
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-04-02 17:15:22 +0300

    uzduotys

---
+++ 08-masyvai/04-uzduotys.js
@@ -0,0 +1,7 @@
+/* Užpildykite masyvą norimais skaičiais ir raidėmis.
+Raskite poziciją skaičiaus, kuris yra lygus 1.
+Raskite poziciją raidės 'a'.
+Raskite antrojo skaičiaus 1 poziciją.
+Raskite kurioje vietoje yra trečiasis skaičius lygus 1.
+*/
+
---
+++ 08-masyvai/06-uzduotys.js
@@ -0,0 +1,10 @@
+/* Susikurkite modulių masyvą. Kuriame saugotumėte
+modulių objektus, kurie susidaro iš šių duomenų:
+- Paskaitos pavadinimas
+- Akademinis kodas
+- Kreditų skaičius
+- Studentų skaičius
+- Vedimo kalba (lt/en/...)
+Raskite:
+- 
+*/
