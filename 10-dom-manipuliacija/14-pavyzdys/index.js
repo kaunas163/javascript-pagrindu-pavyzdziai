a14c39f50b90962a0c61eee65a6546a4e847c64a
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-04-04 16:03:26 +0300

    pavyzdžiai ir užduotys

---
+++ 10-dom-manipuliacija/14-pavyzdys/index.js
@@ -0,0 +1,7 @@
+function pakeisti(elementas, tekstas) {
+    document.getElementById(elementas).innerHTML = tekstas;
+}
+
+function kitaSpalva(elementas, spalva) {
+    document.getElementById(elementas).style.color = spalva;
+}
