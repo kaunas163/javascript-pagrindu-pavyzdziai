4e673f5022da9ebad176483b13ff7d7b2b89386b
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-04-04 18:40:02 +0300

    atnaujinimai

--- 09-funkcijos/09-rest-operator.js
+++ 09-funkcijos/09-rest-operator.js
@@ -1,6 +1,11 @@
 // ...args - rest operator, used with arguments
-function dummy(...args) {
+function dummy(a, b, ...args) {
     console.log(args);
 }
 
+function dummy2(a, b) {
+    console.log(arguments);
+}
+
 dummy(1, 2, 3, 4, 5);
+dummy2(1, 2, 3, 4, 5);
