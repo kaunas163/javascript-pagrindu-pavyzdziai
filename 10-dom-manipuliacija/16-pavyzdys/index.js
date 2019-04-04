1778f70ff018526b1a08fd3d7a447ef1ed7765e7
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-04-04 16:14:49 +0300

    pavyzdžiai ir užduotys

---
+++ 10-dom-manipuliacija/16-pavyzdys/index.js
@@ -0,0 +1,12 @@
+var pirmas = 8;
+var antras = 9;
+
+var suma = pirmas + antras;
+var skirtumas = pirmas - antras;
+var sandauga = pirmas * antras;
+var dalmuo = pirmas / antras;
+
+document.getElementById("suma").innerHTML = pirmas + " + " + antras + " = " + suma;
+document.getElementById("skirtumas").innerHTML = pirmas + " - " + antras + " = " + skirtumas;
+document.getElementById("sandauga").innerHTML = pirmas + " * " + antras + " = " + sandauga;
+document.getElementById("dalmuo").innerHTML = pirmas + " / " + antras + " = " + dalmuo;
---
+++ 10-dom-manipuliacija/17-pavyzdys/index.js
@@ -0,0 +1,9 @@
+let pirmas = 7;
+let antras = 5;
+
+let isvedimui = `<p>${pirmas} + ${antras} = ${pirmas + antras}</p>
+<p>${pirmas} - ${antras} = ${pirmas - antras}</p>
+<p>${pirmas} * ${antras} = ${pirmas * antras}</p>
+<p>${pirmas} / ${antras} = ${pirmas / antras}</p>`;
+
+document.getElementById('turinys').innerHTML = isvedimui;
